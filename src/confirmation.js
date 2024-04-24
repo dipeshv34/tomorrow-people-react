e239033f661404b5798f77916f78b4937fc9137d
Fenil <120449652+FenilDev@users.noreply.github.com>
2024-04-24 23:57:12 +0530

    fixed condition page issue

--- src/confirmation.js
+++ src/confirmation.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Confirmation from './pages/confirmation';
 
 const root = ReactDOM.createRoot(document.getElementById(
-    `confirmation__module--${dataset.moduleInstance}`,
+    `confirmation--${dataset.moduleInstance}`,
 ));
 root.render(<Confirmation />);

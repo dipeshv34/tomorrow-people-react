3ca1691f4998610e7be15887136348987b02c40f
Fenil <120449652+FenilDev@users.noreply.github.com>
2024-04-24 23:59:04 +0530

    fixed confirmation page issue

--- src/confirmation.js
+++ src/confirmation.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Confirmation from './pages/confirmation';
 
-const root = ReactDOM.createRoot(document.getElementById(
-    `confirmation--${dataset.moduleInstance}`,
-));
+const root = ReactDOM.createRoot(document.getElementById(`confirmation--2024`));
 root.render(<Confirmation />);

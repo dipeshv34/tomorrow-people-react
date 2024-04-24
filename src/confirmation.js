bd1cc1554f901a0ef0ed1613e0f3f9c81d6e1332
Fenil <120449652+FenilDev@users.noreply.github.com>
2024-04-25 00:15:05 +0530

    fixed confirmation page render issue

--- src/confirmation.js
+++ src/confirmation.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Confirmation from './pages/confirmation';
 
-const root = ReactDOM.createRoot(document.getElementById(`confirmation--2024`));
+const root = ReactDOM.createRoot(document.getElementById(`root`));
 root.render(<Confirmation />);

0184223ae36f3729f7a3b40a9227a9c36325b4ce
Fenil <120449652+FenilDev@users.noreply.github.com>
2024-04-25 22:29:25 +0530

    fixed confirmation page change

--- src/index.js
+++ src/index.js
@@ -4,17 +4,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const targetModulesData = document.querySelectorAll(
-    '.cms-react-boilerplate > script[type="application/json"]',
-);
-targetModulesData.forEach(({ dataset, textContent }) => {
-    const root = document.getElementById(`App--${dataset.moduleInstance}`);
-    return ReactDOM.render(
-        <App
-            portalId={dataset.portalId}
-            moduleData={JSON.parse(textContent)}
-            moduleInstance={dataset.moduleInstance}
-        />,
-        root,
-    );
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
